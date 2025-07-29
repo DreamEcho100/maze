9e46203145b2b58abb682d264a3ff79169867666
DreamEcho100 <maze6572198@gmail.com>
2025-07-29 21:09:44 +0300

    feat: Update ORPC client configuration

--- apps/volmify-web-client/src/components/header.tsx
+++ apps/volmify-web-client/src/components/header.tsx
@@ -1,27 +1,25 @@
 import { Link } from "@tanstack/solid-router";
-import UserMenu from "./user-menu";
 import { For } from "solid-js";
+import UserMenu from "./user-menu";
 
 export default function Header() {
-  const links = [
-    { to: "/", label: "Home" },
-    { to: "/dashboard", label: "Dashboard" },
-    { to: "/todos", label: "Todos" },
-  ];
+	const links = [
+		{ to: "/" as const, label: "Home" },
+		{ to: "/dashboard" as const, label: "Dashboard" },
+		{ to: "/todos" as const, label: "Todos" },
+	];
 
-  return (
-    <div>
-      <div class="flex flex-row items-center justify-between px-2 py-1">
-        <nav class="flex gap-4 text-lg">
-          <For each={links}>
-            {(link) => <Link to={link.to}>{link.label}</Link>}
-          </For>
-        </nav>
-        <div class="flex items-center gap-2">
-          <UserMenu />
-        </div>
-      </div>
-      <hr />
-    </div>
-  );
+	return (
+		<div>
+			<div class="flex flex-row items-center justify-between px-2 py-1">
+				<nav class="flex gap-4 text-lg">
+					<For each={links}>{(link) => <Link to={link.to}>{link.label}</Link>}</For>
+				</nav>
+				<div class="flex items-center gap-2">
+					<UserMenu />
+				</div>
+			</div>
+			<hr />
+		</div>
+	);
 }
--- apps/volmify-web-client/src/utils/orpc.ts
+++ apps/volmify-web-client/src/utils/orpc.ts
@@ -1,26 +1,26 @@
 import { createORPCClient } from "@orpc/client";
 import { RPCLink } from "@orpc/client/fetch";
+import type { RouterClient } from "@orpc/server";
 import { createTanstackQueryUtils } from "@orpc/tanstack-query";
 import { QueryCache, QueryClient } from "@tanstack/solid-query";
-import type { appRouter } from "../../../server/src/routers/index";
-import type { RouterClient } from "@orpc/server";
+import type { appRouter } from "../../../volmify-main-server/src/routers/index";
 
 export const queryClient = new QueryClient({
-  queryCache: new QueryCache({
-    onError: (error) => {
-      console.error(`Error: ${error.message}`);
-    },
-  }),
+	queryCache: new QueryCache({
+		onError: (error) => {
+			console.error(`Error: ${error.message}`);
+		},
+	}),
 });
 
 export const link = new RPCLink({
-  url: `${import.meta.env.VITE_SERVER_URL}/rpc`,
-  fetch(url, options) {
-    return fetch(url, {
-      ...options,
-      credentials: "include",
-    });
-  },
+	url: `${import.meta.env.VITE_SERVER_URL}/rpc`,
+	fetch(url, options) {
+		return fetch(url, {
+			...options,
+			credentials: "include",
+		});
+	},
 });
 
 export const client: RouterClient<typeof appRouter> = createORPCClient(link);
